90a32dad4f0f0c168e3c70e7cd4c4975ccf09854
razluman <razluman@gmail.com>
2023-05-07 10:28:32 +0300

    nouveau fichier : import_data.py

---
+++ import_data.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from .models import Rate, Currency
+
+
+def import_rate():
+    user_id = input("Enter user id : ")
+    user = User.objects.get(pk=user_id)
+    res = input(f"Import with {user} ? ")
+    if res.upper() != "O":
+        print("Operation cancelled")
+        return
+    filename = "Rate-2023-05-07.csv"
+    i = 1
+    sep = "\t"
+    with open(filename, "r") as file:
+        for line in file:
+            line = line.replace("\n", "")
+            data = line.split(sep)
+            date = data[1]
+            rate = data[2]
+            currency = Currency.objects.get(pk=data[3])
+            Rate.objects.create(
+                date=date,
+                rate=rate,
+                currency=currency,
+                created_by=user,
+                updated_by=user,
+            )
+            print(i)
+            i = i + 1
+    print("Finished")
+
+
+import_rate()
